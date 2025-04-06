899586d0e74c46185d2b857b5aa3d46ef6937da3
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-04-06 15:52:01 +0700

    fix(data): changing hardcode data to using props to passing data

--- contact-component/src/App.jsx
+++ contact-component/src/App.jsx
@@ -1,10 +1,37 @@
-
 import Contact from "./Contact"
+
+import MrWhikersonImg from "./assets/mr-whiskerson.jpg"
+import FelixImg from "./assets/felix.jpg"
+import FluffykinsImg from "./assets/fluffykins.jpg"
+import PumpkinImg from "./assets/pumpkin.jpg"
 
 function Main() {
   return (
       <div className="contacts">
-        <Contact />          
+        <Contact
+          img={MrWhikersonImg}
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact
+          img={FelixImg}
+          name="Fluffykins"
+          phone="(212) 555-2345"
+          email="fluff@me.com"
+        />
+        <Contact
+          img={FluffykinsImg}
+          name="Felix"
+          phone="(212) 555-4567"
+          email="thecat@hotmail.com"
+        />
+        <Contact 
+          img={PumpkinImg}
+          name="Pumpkin"
+          phone="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+        />         
       </div>
   )
 }
