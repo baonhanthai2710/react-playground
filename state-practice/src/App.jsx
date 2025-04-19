15f4d0fd207299abd996a51e3b77741532ea0b79
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-04-19 11:32:12 +0700

    feat: updateing state with a callback function

--- state-practice/src/App.jsx
+++ state-practice/src/App.jsx
@@ -1,23 +1,22 @@
 import React from "react"
 
 export default function App() {
-    let [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0)
 
     function add () {
-        setCount(count + 1)
+        setCount((prevCount) => prevCount + 1)
     }
 
-    function minus () {
-        setCount(count - 1)
+    function subtract () {
+        setCount((prevCount) => prevCount - 1)
     }
-
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button className="minus" aria-label="Decrease count" onClick={minus} >–</button>
+                <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
                 <h2 className="count">{count}</h2>
-                <button className="plus" aria-label="Increase count" onClick={add} >+</button>
+                <button className="plus" onClick={add} aria-label="Increase count">+</button>
             </div>
         </main>
     )
